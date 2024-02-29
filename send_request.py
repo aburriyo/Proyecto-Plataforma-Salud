9954efba13912de062baed1b606343c933e0d92f
aburriyo <53106596+aburriyo@users.noreply.github.com>
2024-02-29 16:14:25 -0300

    Delete send_request.py

--- send_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api-beta.codegpt.co/api/v1/agent/id"
-
-headers = {"accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
